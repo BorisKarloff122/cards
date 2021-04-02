a171de501979bef17ee1f106e01d28b98974f040
BorisKarlof <boriskarlof122@gmail.com>
2021-04-02 13:42:37 +0300

    Boris Karloff
    added basic components, added page components, added basic modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { CollectionComponent } from './pages/collection/collection.component';
+import { PlayComponent } from './pages/play/play.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardComponent } from './shared/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CollectionComponent,
+    PlayComponent,
+    CardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
